e0dd97298c01a9f7c4f2905af82e17c32cf3ac2b
Anastasia C <clarkanastasia@outlook.com>
2024-03-07 14:02:50 +0000

    change DateTime to string

--- Models/Response/AnimalResponse.cs
+++ Models/Response/AnimalResponse.cs
@@ -3,10 +3,10 @@ namespace ZooManagement.Models.Response;
 public class AnimalResponse
 {
     public required string Name { get; set; }
-    public required string SpeciesName { get; set; }
+    public required string Species { get; set; }
     public required string Classification { get; set; }
     public required string Sex { get; set; }
     public required string Enclosure {get;set;}
-    public DateTime? DateOfBirth { get; set; }
-    public required DateTime DateOfAcquisition { get; set; }
+    public string? DateOfBirth { get; set; }
+    public required string DateOfAcquisition { get; set; }
 }
