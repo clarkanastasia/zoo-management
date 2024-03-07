541abdcc8e9625a2de474db8d93144a722ae96d6
Anastasia C <clarkanastasia@outlook.com>
2024-03-07 13:39:10 +0000

    install Newtonsoft.Json

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using System.Text.Json.Serialization;
 using ZooManagement;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options => 
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddDbContext<Zoo>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
