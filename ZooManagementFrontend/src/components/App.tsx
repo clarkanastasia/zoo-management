f191cfc2871354731a2f4639df37c3d5133b7a01
Anastasia C <clarkanastasia@outlook.com>
2024-03-08 17:56:12 +0000

    add routes

--- ZooManagementFrontend/src/components/App.tsx
+++ ZooManagementFrontend/src/components/App.tsx
@@ -1,10 +1,29 @@
 import '../styles/App.css'
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import Home from './Home'
+import NavBar from './NavBar'
+import AnimalDetail from './AnimalDetail'
+import EnclosureDetail from './EnclosureDetail'
+
 
 const App: React.FC = () => {
 
   return (
-    <>
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path = '/' element = {<Home/>}/>
+        <Route path = '/animals/animal/:animalId' element = {<AnimalDetail/>} />
+        <Route path = '/enclosure/:enclosureId' element = {<EnclosureDetail/>} />
+
+        <Route path = '*' element = {
+          <div>
+            <h1>Oops, this page doesn't exist. Let's get you back to the home page:</h1>
+            <Link to = '/'>Home</Link>
+          </div>
+        }/>
+      </Routes>
+    </Router>
   )
 }
 
