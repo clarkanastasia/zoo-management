c117f93e41955b3a448abf32302d137ecac04f56
Anastasia C <clarkanastasia@outlook.com>
2024-03-26 21:01:58 +0000

    update routes

--- ZooManagementFrontend/src/components/App.tsx
+++ ZooManagementFrontend/src/components/App.tsx
@@ -1,10 +1,12 @@
-import '../styles/App.css'
-import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
+import "../main.scss"
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { LinkContainer } from "react-router-bootstrap"
+import { Button } from "react-bootstrap"
 import Home from './Home'
 import NavBar from './NavBar'
 import AnimalDetail from './AnimalDetail'
 import EnclosureDetail from './EnclosureDetail'
-
+import AllAnimals from "./AllAnimals"
 
 const App: React.FC = () => {
 
@@ -15,11 +17,13 @@ const App: React.FC = () => {
         <Route path = '/' element = {<Home/>}/>
         <Route path = '/animals/animal/:animalId' element = {<AnimalDetail/>} />
         <Route path = '/enclosure/:enclosureId' element = {<EnclosureDetail/>} />
-
+        <Route path = '/animals/all' element= {<AllAnimals />}/>
         <Route path = '*' element = {
-          <div>
-            <h1>Oops, this page doesn't exist. Let's get you back to the home page:</h1>
-            <Link to = '/'>Home</Link>
+          <div className="d-flex flex-column align-items-center">
+            <h1 className="text-center">Oops, this page doesn't exist.<br/> Let's get you back to the home page:</h1>
+            <LinkContainer to="/">
+              <Button size="lg">Home</Button>
+            </LinkContainer>
           </div>
         }/>
       </Routes>
