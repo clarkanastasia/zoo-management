26237f69f730184faf6cb0ebd41fab478eab60f5
Anastasia C <clarkanastasia@outlook.com>
2024-03-26 19:55:26 +0000

    update navbar

--- ZooManagementFrontend/src/components/NavBar.tsx
+++ ZooManagementFrontend/src/components/NavBar.tsx
@@ -1,10 +1,29 @@
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap"
+
 const NavBar: React.FC = () => {
 
     return (
-        <nav>
-
-        </nav>
-    )
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+            <Navbar.Brand href="/">Zoo Management</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/animals/all">All Animals</Nav.Link>
+                <NavDropdown title="Enclosures" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="/enclosure/-1">Giraffe</NavDropdown.Item>
+                    <NavDropdown.Item href="/enclosure/-2">Aviary</NavDropdown.Item>
+                    <NavDropdown.Item href="/enclosure/-3">Reptile</NavDropdown.Item>
+                    <NavDropdown.Item href="/enclosure/-4">Reptile</NavDropdown.Item>
+                    <NavDropdown.Item href="/enclosure/-5">Hippo</NavDropdown.Item>
+                    <NavDropdown.Item href="/enclosure/-6">Lion</NavDropdown.Item>
+                </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+        );
     }
 
 export default NavBar
