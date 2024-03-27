e246ebbfefc1c1951bd370d239012d14d005f082
Anastasia C <clarkanastasia@outlook.com>
2024-03-27 20:56:12 +0000

    update animal detail component

--- ZooManagementFrontend/src/components/AnimalDetail.tsx
+++ ZooManagementFrontend/src/components/AnimalDetail.tsx
@@ -1,40 +1,50 @@
 import {useState, useEffect} from 'react'
 import {animalModel} from '../models/animalModel'
 import {useParams} from 'react-router-dom'
+import { Button, Card, CardBody, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const AnimalDetail: React.FC = () => {
     const { animalId } = useParams();
     const URL = `http://localhost:5031/animals/animal/${animalId}`;
-    const [myData, setData] = useState<animalModel|null>(null);
+    const [animalInfo, setAnimalInfo] = useState<animalModel>();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         fetch(URL)
         .then(response => response.json())
         .then((data) => {
-            setData(data);
+            setAnimalInfo(data);
         })
-        .catch(error => {
-            console.log(error);
-        })
-            
+        .catch(() => setError(true))
+        .finally(() => setLoading(false))      
     }, []);
 
-    if(myData == null){
-        return <h1>Loading data</h1>
-    }
-
     return (
-        <div>
-            <ul>
-                <li>Name: {myData.name}</li>
-                <li>Species: {myData.species}</li>
-                <li>Classification: {myData.classification}</li>
-                <li>Sex: {myData.sex}</li>
-                <li>Enclosure: {myData.enclosure}</li>
-                {myData.dateOfBirth && (
-                <li> Date of Birth: {myData.dateOfBirth}</li>)}
-                <li>Date of Acquisition: {myData.dateOfAcquisition}</li>
-            </ul>
+        <div className="d-flex flex-column align-items-center text-center" style={{marginTop:"5rem"}}>
+            {animalInfo && (
+                <Card style={{ width: '20rem' }}>
+                    <CardBody>
+                    <Card.Title>{animalInfo.name}</Card.Title>
+                        <ListGroup>
+                            <ListGroupItem>Species: {animalInfo.species}</ListGroupItem>
+                            <ListGroupItem>Classification: {animalInfo.classification}</ListGroupItem>
+                            <ListGroupItem>Sex: {animalInfo.sex}</ListGroupItem>
+                            {animalInfo.dateOfBirth && (
+                                <ListGroupItem>Date of Birth: {animalInfo.dateOfBirth}</ListGroupItem>
+                            )}
+                            <ListGroupItem>Date of Acquisition: {animalInfo.dateOfAcquisition}</ListGroupItem>
+                        </ListGroup>
+                        <LinkContainer to={`/enclosure/${animalInfo.enclosureId}`}>
+                            <Button className="mt-3">Back to Enclosure</Button>
+                        </LinkContainer>
+                        </CardBody>
+
+                </Card>
+            )}
+            {loading && <p>Loading...</p>}
+            {error && <p>Unable to load data at this time</p>}
         </div>
     )
     }
